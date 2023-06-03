7894973477192dcdd4d537eec5332036dd3fad0f
ravioles33 <99939387+ravioles33@users.noreply.github.com>
2023-06-03 20:38:18 -0300

    viewsss

--- event/views.py
+++ event/views.py
@@ -7,14 +7,22 @@ from event.serializer import EventSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
 from event.models import Event
+from rest_framework import filters
 
 class UserViewSet(viewsets.ViewSet):
     """
     A simple ViewSet for listing or retrieving users.
     """
     def list(self, request):
-        queryset = Event.objects.all() #bbdd
-        serializer = EventSerializer(queryset, many=True) #transforma de obj
+        if len(request.data.keys()) == 0:
+            
+            queryset = Event.objects.all() #bbdd
+            serializer = EventSerializer(queryset, many=True) #transforma de obj
+            return Response(serializer.data) #retorna respuesta
+        
+        filters = request.data
+        result = Event.objects.filter(start_date=filters['start_date'])
+        serializer = EventSerializer(result, many=True) #transforma de obj
         return Response(serializer.data) #retorna respuesta
     
     def create(self, request):
@@ -23,4 +31,6 @@ class UserViewSet(viewsets.ViewSet):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+    
+
     
