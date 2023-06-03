5e74c22dcc40da7c23651ea7a7c853dd89bf4e3e
ravioles33 <99939387+ravioles33@users.noreply.github.com>
2023-06-03 19:50:06 -0300

    vamos con el serializer

--- event/serializer.py
+++ event/serializer.py
@@ -6,3 +6,5 @@ class EventSerializer(serializers.ModelSerializer):
         model=Event
         #fields=('fullname','nickname') #Indicando los campos individualmente
         fields = '__all__' #todos los campos
+
+
--- event/views.py
+++ event/views.py
@@ -16,3 +16,11 @@ class UserViewSet(viewsets.ViewSet):
         queryset = Event.objects.all() #bbdd
         serializer = EventSerializer(queryset, many=True) #transforma de obj
         return Response(serializer.data) #retorna respuesta
+    
+    def create(self, request):
+        serializer = EventSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+    
