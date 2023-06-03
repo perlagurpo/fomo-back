51133e23d8206150133ff64016dbd7d6b426909a
ravioles33 <99939387+ravioles33@users.noreply.github.com>
2023-06-03 18:11:15 -0300

    modelando event

---
+++ event/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Event(models.Model):
+    # Atributos de la clase Event
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(null=True)
+    event_name = models.CharField(max_length=255)
+    event_type = models.CharField(max_length=255)
+    has_ticket = models.BooleanField(null=True)
+    ticket_price = models.IntegerField(null=True)
+    tickets_left = models.BooleanField(null=True)
+    tickets_available = models.IntegerField(null=True)
+    buy_tickets = models.CharField(max_length=255)
+    event_link = models.CharField(max_length=255)
+    event_img = models.CharField(max_length=255)
+    organization_page = models.CharField(max_length=255)
+    
+
+    def __str__(self):
+        return self.nombre
