8f47f6813ddae63bd59218dc481d42f51d3adb63
ravioles33 <99939387+ravioles33@users.noreply.github.com>
2023-06-24 18:56:44 -0300

    habemus filtro rango fechas

--- event/services.py
+++ event/services.py
@@ -9,9 +9,18 @@ def filtro_fecha_exacta(data, query_set):
     filters = data 
 
     if 'start_date' in filters.keys():
-        fecha_start = filters['start_date']
-        fecha_start_obj = datetime.strptime(fecha_start, '%d-%m-%Y')
-        event_filter_qs = query_set.filter(start_date=fecha_start_obj)
+        start_date = filters['start_date']
+        print(start_date)
+        if type(start_date) == list:
+            date_1 = start_date[0]
+            date_2 = start_date[1]
+            date_1 = datetime.strptime(date_1, '%d-%m-%Y')
+            date_2 = datetime.strptime(date_2, '%d-%m-%Y')
+            event_filter_qs = query_set.filter(start_date__range=[date_1, date_2])
+        else:
+            fecha_start = filters['start_date']
+            fecha_start_obj = datetime.strptime(fecha_start, '%d-%m-%Y')
+            event_filter_qs = query_set.filter(start_date=fecha_start_obj)
         return event_filter_qs
 
 def filtro_event_name_contain(data, query_set):
