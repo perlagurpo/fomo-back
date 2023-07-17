7e66db9259ec7f07a2d5fa3fd3b4d7dc8ad2bb50
ravioles33 <juliann.mdq@gmail.com>
2023-07-17 01:23:43 -0300

    intentando usuarios

--- event/models.py
+++ event/models.py
@@ -17,6 +17,7 @@ class Event(models.Model):
     organization_page = models.CharField(max_length=255, null=True)
     event_location = models.CharField(max_length=255, null=True)
 
-"""     
-    def __str__(self):
-        return self.event_date """
+    owner = models.ForeignKey('auth.user', related_name='events') #on_delete=models.CASCADE?indica que si un usuario se elimina, todos sus fragmentos de código también se eliminarán.
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
--- event/serializer.py
+++ event/serializer.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import Event
 
+from django.contrib.auth.models import User
+
+
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model=Event
@@ -8,3 +11,9 @@ class EventSerializer(serializers.ModelSerializer):
         fields = '__all__' #todos los campos
 
 
+class UserSerializer(serializers.ModelSerializer):
+    events = serializers.PrimaryKeyRelatedField(many=True, queryset=Event.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', ''] #??
