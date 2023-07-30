0d18e50961734e8c00a5631d64ddce607b9decf8
ravioles33 <juliann.mdq@gmail.com>
2023-07-30 13:15:07 -0300

    sirve imágenes en url pública

--- settings/urls.py
+++ settings/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('event/', include('event.urls')),
+]
+# Ruta para servir imágenes desde MEDIA_ROOT
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
