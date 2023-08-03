b689b41362ecee27c5f7226bc3a17710998bf455
ravioles33 <juliann.mdq@gmail.com>
2023-08-02 21:29:55 -0300

    day_names en español

--- category/urls.py
+++ category/urls.py
@@ -7,7 +7,6 @@ from category.views import CategoryListView, CategoryDetailView
 urlpatterns = [
     path(r'', CategoryListView.as_view(), name='category_list_view'),
     path(r'<int:pk>/', CategoryDetailView.as_view(), name='category_list_view')
-
 ]
 
 
--- event/models.py
+++ event/models.py
@@ -43,8 +43,8 @@ class Event(models.Model):
     def day_name_end(self):
         if self.start_date:
             # Establece idioma español
-            #locale.setlocale(locale.LC_ALL, 'es_AR.utf8') #validar con el "locale -a"
-            #locale.getlocale(locale.LC_TIME)
+            locale.setlocale(locale.LC_ALL, 'es_AR.utf8') #validar con el "locale -a"
+            locale.getlocale(locale.LC_TIME)
             # Formatear la fecha y obtener el nombre del día... ahora en es
             return self.end_date.strftime('%A').capitalize()
         return None
