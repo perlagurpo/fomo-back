29b6c668cd57d9c92bfcc799895b07573672279e
ravioles33 <juliann.mdq@gmail.com>
2023-08-25 11:17:55 -0300

    modelo category agregado verbose_name

--- category/models.py
+++ category/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, null=True, blank=True, unique=True)
+    name = models.CharField(max_length=255, null=True, blank=True, unique=True, verbose_name='Nombre')
 
     def __str__(self):
         return self.name
